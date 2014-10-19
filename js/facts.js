71ad3a6c177e679daf66e5da91f98c95bbf254c1
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-10-19 13:39:54 -0700

    bring under git

---
+++ js/facts.js
@@ -0,0 +1,16 @@
+var facts = [
+	'Loons are my favorite animal.',
+	'If I could I would only eat avocados for the rest of my life.',
+	'I read trivia books for fun.',
+	'<a href="http://www.amazon.com/Quickword-Ultimate-Green-Spinner-Scorecard/dp/0880795107/" target="_blank">Quickword</a> is my favorite board game.',
+	'Calculus is my favorite branch of mathmatics.',
+	'Logic puzzles were my favorite part of fourth grade.',
+	'<a href="http://www.protrails.com/trails/view/16/boulder_denver_golden_fort_collins_lyons/green_mountain_summit_via_the_ranger_trail" target="_blank">Green Mountain</a> is my favorite short hike.',
+	'Caffeine makes up more of my blood stream than blood.',
+	'I think <a href="http://www.bobsredmill.com/steel-cut-oats.html" target="_blank">Bob&rsquo;s Red Mill Steel Cut Oats</a> are the Bomb.com.',
+	'NPR is my favorite radio station.',
+	'<a href="http://www.maththeband.com/" target="_blank">Math The Band</a> was the first band I saw in concert.',
+	'U+1F603 is my favorite unicode character.',
+	'If you refresh this page enough times, you can answer all my security questions.'
+	/* "You're a cheater if you're reading this." */
+];
